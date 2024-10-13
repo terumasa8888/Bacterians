8e2f5894e32c29a58d2045748c1d97ad418b5555
DESKTOP-EESACKJ\nakamuraT2020 <terumasa8888@gmail.com>
2024-10-13 23:31:54 +0900

    add: NavMeshAgent2Dに停止、再開処理を追加

--- Assets/Scripts/NavMeshAgent2D.cs
+++ Assets/Scripts/NavMeshAgent2D.cs
@@ -1,44 +1,65 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NavMeshAgent2D : MonoBehaviour {
+public class NavMeshAgent2D : MonoBehaviour
+{
     [Header("Steering")]
     public float speed = 1.0f;
     public float stoppingDistance = 0;
 
     [HideInInspector] // 常にUnityエディタから非表示
     private Vector2 trace_area = Vector2.zero;
-    public Vector2 destination {
+    private float originalSpeed;
+
+    public Vector2 destination
+    {
         get { return trace_area; }
-        set {
+        set
+        {
             trace_area = value;
             Trace(transform.position, value);
         }
     }
-    public bool SetDestination(Vector2 target) {
+
+    public bool SetDestination(Vector2 target)
+    {
         destination = target;
         return true;
     }
 
-    private void Trace(Vector2 current, Vector2 target) {
-        if (Vector2.Distance(current, target) <= stoppingDistance) {
+    public void Stop()
+    {
+        originalSpeed = speed;
+        speed = 0;
+    }
+
+    public void Resume()
+    {
+        speed = originalSpeed;
+    }
+
+    private void Trace(Vector2 current, Vector2 target)
+    {
+        if (Vector2.Distance(current, target) <= stoppingDistance)
+        {
             return;
         }
 
         // NavMesh に応じて経路を求める
         NavMeshPath path = new NavMeshPath();
-        //Debug.Log($"Calculating path from {current} to {target}");
-        if (NavMesh.CalculatePath(current, target, NavMesh.AllAreas, path) && path.corners.Length > 0) {
+        if (NavMesh.CalculatePath(current, target, NavMesh.AllAreas, path) && path.corners.Length > 0)
+        {
             Vector2 corner = path.corners[0];
 
-            if (path.corners.Length > 1 && Vector2.Distance(current, corner) <= 0.05f) {
+            if (path.corners.Length > 1 && Vector2.Distance(current, corner) <= 0.05f)
+            {
                 corner = path.corners[1];
             }
 
             transform.position = Vector2.MoveTowards(current, corner, speed * Time.deltaTime);
-            //Debug.Log($"Moving towards: {corner}");
         }
-        else {
+        else
+        {
             Debug.LogWarning("Path calculation failed or no corners found.");
         }
     }
