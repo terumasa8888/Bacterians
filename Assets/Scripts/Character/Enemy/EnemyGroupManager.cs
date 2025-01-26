2218f8fc38661a4826045a810514be98464a7192
DESKTOP-EESACKJ\nakamuraT2020 <m24j4045a@edu.tottori-u.ac.jp>
2025-01-27 02:53:55 +0900

    refactor: ArgumentOutOfRangeExceptionの修正

--- Assets/Scripts/Character/Enemy/EnemyGroupManager.cs
+++ Assets/Scripts/Character/Enemy/EnemyGroupManager.cs
@@ -98,6 +98,13 @@ public class EnemyGroupManager : MonoBehaviour
 
     Dictionary<int, List<Transform>> ClusterObjects(List<Transform> objects, int clusterCount)
     {
+        Dictionary<int, List<Transform>> clusters = new Dictionary<int, List<Transform>>();
+
+        if (objects.Count == 0)
+        {
+            return clusters;
+        }
+
         // 初期のセントロイドをランダムに選択
         List<Vector3> centroids = new List<Vector3>();
         for (int i = 0; i < clusterCount; i++)
@@ -106,7 +113,6 @@ public class EnemyGroupManager : MonoBehaviour
             centroids.Add(objects[Random.Range(0, objects.Count)].position);
         }
 
-        Dictionary<int, List<Transform>> clusters = new Dictionary<int, List<Transform>>();
         bool hasChanged;
 
         do
